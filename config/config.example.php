e47cdfab4d78412ab269f9ba7f5925a60e18bed6
Japexx19 <147778513+Japexx19@users.noreply.github.com>
2026-08-17 20:43:04 -0300

    Add files via upload

---
+++ config/config.example.php
@@ -0,0 +1,13 @@
+<?php
+// Copie para config.php e preencha com os dados do MySQL da Hostinger.
+// NÃO publique config.php no GitHub.
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'NOME_DO_BANCO',
+    'db_user' => 'USUARIO_DO_BANCO',
+    'db_pass' => 'SENHA_DO_BANCO',
+    'api_key' => 'TROQUE_POR_UMA_CHAVE_GRANDE_E_ALEATORIA',
+    'store_name' => 'TH Lanches',
+    'store_phone' => '',
+    'store_address' => '',
+];
